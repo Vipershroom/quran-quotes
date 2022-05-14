d96df41a8874babdee80d6c926b3cde2bf93f57f
Vipershroom <undertalefan812@gmail.com>
2022-05-13 22:36:03 -0400

    Moved components to App.tsx

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+import Verse from './Verse'
+
+const App = () => {
+  return (
+    <div>
+        <Header />
+        <Verse />
+    </div>
+  )
+}
+
+export default App
