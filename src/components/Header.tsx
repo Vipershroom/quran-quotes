f3115d050ef3cf4afe85dfa9a40aef39e9b0064c
Vipershroom <undertalefan812@gmail.com>
2022-05-13 23:31:29 -0400

    Displayed curreny verse

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../styles/HeaderMod.scss'
 
 function App() {
   return (
       <>
-      <h1>Your Verse of the day is:</h1>
+      <h1 id='header'>Your Verse of the day is:</h1>
       </>
 )}
 
--- src/components/Verse.tsx
+++ src/components/Verse.tsx
@@ -1,16 +1,52 @@
 import React, { useEffect, useState } from 'react'
 import '../styles/VerseMod.scss'
 
+const rand = () => {
+  return Math.floor((Math.random() * 6236) + 1)
+}
+
+const splitWords = (sentence: string) => {
+  let n = sentence.split(' ')
+  let count = 0
+  let str = ""
+  for (let i of n) {
+    if (count === 10) {
+      str += '\n'
+      count = 0
+    }
+    str += ' ' + i
+    count++
+  }
+  return str
+}
+
 const Verse = () => {
 
-  const [verse, setVerse] = useState("Placeholder")
+  const [verse, setVerse] = useState("place")
+  const [verseAuth, setVerseAuth] = useState("place")
 
-  // useEffect
+  useEffect(() => {
+    const verseNum = rand()
+    const url = `http://api.alquran.cloud/v1/ayah/${verseNum}/editions/quran-uthmani,en.asad,en.pickthall`
+    const data = fetch(url)
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      let m = splitWords(data['data'][1]['text'])
+      console.log(splitWords(data['data'][1]['text']))
+      setVerse(m)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+
+  }, [])
 
   return (
     <div id='center'>
         <h2>{verse}</h2>
-        <p>damn</p>
+        <p>{verseAuth}</p>
     </div>
   )
 }
